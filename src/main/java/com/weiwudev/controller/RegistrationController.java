ac956147bfcbca065a3f1ed11c2d5cb9c52c9183
Wei Wu <60204220+weiwu1994@users.noreply.github.com>
2020-07-22 18:43:38 -0400

    test

--- src/main/java/com/weiwudev/controller/RegistrationController.java
+++ src/main/java/com/weiwudev/controller/RegistrationController.java
@@ -3,6 +3,8 @@ package com.weiwudev.controller;
 import com.weiwudev.models.User;
 import com.weiwudev.services.RegistrationService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +17,10 @@ public class RegistrationController {
     RegistrationService registrationService;
 
     @PostMapping("/register")
-    public Mono<String> registerUser(@RequestBody User user){
-        return registrationService.registerUser(user);
+    public Mono<ResponseEntity<String>> registerUser(@RequestBody User user){
+
+        return registrationService.registerUser(user).flatMap(data -> Mono.just(ResponseEntity.status(HttpStatus.OK).body(data))).onErrorReturn(
+                ResponseEntity.status(HttpStatus.CONFLICT).body("username already exist"));
     }
     @PostMapping("/check")
     public Mono<String> check(@RequestBody User user){
